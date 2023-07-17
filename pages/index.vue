462c869be44fb8e52115fd0e7af19587adb508cc
RafiAthallahN <rafiraikaza@gmail.com>
2023-07-18 06:34:54 +0700

    first

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+</template>
+
+
+<script setup>
+
+</script>
+
+<style scoped>
+  /* h2 {
+    margin-bottom: 20%;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px 0;
+  } */
+</style>
